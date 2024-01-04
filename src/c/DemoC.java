14ce516b5e461320dc099f887148a38aa964526b
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-01-03 21:03:28 -0500

    initial commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,15 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This class C");
+		
+		System.out.println("Tuesday");
+		System.out.println("Commit to local and remote repository");
+		
+		
+	}
+
+}
